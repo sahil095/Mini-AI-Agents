f2f7ba7906afb03c331a4cd870e124b82a0da689
sahil095 <sehgal.sahil0786@gmail.com>
2026-02-11 23:56:05 -0500

    poetry changes

---
+++ Stock Analysis/src/crew.py
@@ -0,0 +1,130 @@
+import os
+
+from typing import List
+from crewai import Agent, Crew, Task, Process, LLM  # pyright: ignore[reportMissingImports]
+from crewai.project import CrewBase, agent, crew, task
+
+from tools.sec_tools import SEC10KTool, SEC10QTool
+from tools.calculator_tool import CalculatorTool
+
+from crewai_tools import WebsiteSearchTool, ScrapeWebsiteTool, TXTSearchTool
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.llms import Ollama
+# llm = Ollama(model="llama3.1")
+
+llm = LLM(
+    model="groq/llama-3.3-70b-versatile",
+    temperature=0.5,
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1",
+)
+
+@CrewBase
+class StockAnalysisCrew:
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
+
+    @agent
+    def financial_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['financial_analyst'],
+            llm=llm,
+            tools=[
+                ScrapeWebsiteTool(),
+                WebsiteSearchTool(),
+                CalculatorTool(),
+                SEC10KTool(stock_name="AMZN"),
+                SEC10QTool(stock_name="AMZN"),
+            ],
+            verbose=True
+        )
+    
+    @task
+    def financial_analysis(self) -> Task:
+        return Task(
+            config=self.tasks_config['financial_analysis'],
+            agent=self.financial_agent(),
+        )
+    
+    @agent
+    def research_analyst_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['research_analyst'],
+            llm=llm,
+            tools=[
+                ScrapeWebsiteTool(),
+                # WebsiteSearchTool(),
+                SEC10KTool(stock_name="AMZN"),
+                SEC10QTool(stock_name="AMZN"),
+            ],
+            verbose=True
+        )
+
+    @task
+    def research(self) -> Task:
+        return Task(
+            config=self.tasks_config['research'],
+            agent=self.research_analyst_agent(),
+        )
+    
+    @agent
+    def financial_analyst_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['filing_analyst'],
+            llm=llm,
+            tools=[
+                ScrapeWebsiteTool(),
+                WebsiteSearchTool(),
+                CalculatorTool(),
+                SEC10KTool(),
+                SEC10QTool(),
+            ],
+            verbose=True
+        )
+    
+    @task
+    def financial_analysis(self) -> Task: 
+        return Task(
+            config=self.tasks_config['financial_analysis'],
+            agent=self.financial_analyst_agent(),
+        )
+    
+    @task
+    def filings_analysis(self) -> Task:
+        return Task(
+            config=self.tasks_config['filings_analysis'],
+            agent=self.financial_analyst_agent(),
+        )
+    
+    @agent
+    def investment_advisor_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['investment_advisor'],
+            llm=llm,
+            tools=[
+                ScrapeWebsiteTool(),
+                WebsiteSearchTool(),
+                CalculatorTool(),
+            ],
+            verbose=True
+        )
+    
+    @task
+    def recommend(self) -> Task:
+        return Task(
+            config=self.tasks_config['recommend'],
+            agent=self.investment_advisor_agent(),
+        )
+    
+    @crew
+    def crew(self) -> Crew:
+        """Creates a crew with the agents and tasks"""
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            process=Process.sequential,
+            verbose=True
+        )
