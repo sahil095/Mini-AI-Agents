257e51b15a72ef52e80103620ce6a372681e2086
sahil095 <sehgal.sahil0786@gmail.com>
2026-02-11 22:29:56 -0500

    SEC 10-Q filings code

--- Stock Analysis/src/tools/sec_tools.py
+++ Stock Analysis/src/tools/sec_tools.py
@@ -62,7 +62,7 @@ class SEC10KTool(RagTool):
             url = filings[0]['linkToFilingDetails']
 
             headers = {
-                "User-Agent": "",
+                "User-Agent": "sehgal.sahil0786@gmail.com",
                 "Accept-Encoding": "gzip, deflate",
                 "Host": "www.sec.gov"
             }
@@ -88,4 +88,89 @@ class SEC10KTool(RagTool):
 
     def _run(self, search_query: str, **kwargs: Any) -> Any:
         """ Perform semantic search on the 10-K content for the given query"""
+        return super()._run(search_query, **kwargs)
+
+
+class FixedSEC10QToolSchema(BaseModel):
+    """Input schema for FixedSEC10QTool."""
+
+    search_query: str = Field(..., description="Mandatory query you would like to search from the 10-Q filing report.")
+
+
+class SEC10QToolSchema(FixedSEC10QToolSchema):
+    """Input schema for SEC10QTool."""
+    stock_name: str = Field(..., description="Mandatory valid stock name you would like to search")
+
+
+class SEC10QTool(RagTool):
+    name: str = "Search in the specified 10-Q form"
+    description: str = "Useful tool to perform semantic search a query from 10-Q filing report of a company."
+    args_schema: Type[BaseModel] = SEC10QToolSchema
+
+    def __init__(self, stock_name: Optional[str] = None, **kwargs):
+        """ Initialize the SEC10QTool with the given stock name"""
+        print(f"Initializing SEC10QTool with stock_name: {stock_name}")
+        super().__init__(**kwargs)
+        if stock_name is not None:
+            content = self.get_10q_url_content(stock_name)
+            if content:
+                self.add(content)
+                self.description = f"A tool that can be used to semantic search a query from {stock_name}'s latest 10-Q SEC form's content as a txt file."
+                self.args_schema = FixedSEC10QToolSchema
+                self._generate_description()
+    
+    def get_10q_url_content(self, stock_name: str) -> Optional[str]:
+        """ Fetches the URL content as txt of the latest 10-Q form for the given stock""" 
+        try:
+            query_api = QueryApi(api_key=os.getenv("SEC_API_API_KEY"))
+            query = {
+                "query": {
+                    "query_string": {
+                            "query": f"ticker:{stock_name} AND formType:\"10-Q\"",
+                        }
+                    },
+                    "from": 0,
+                    "size": 1,
+                    "sort": [
+                        {
+                            "filedAt": {
+                                "order": "desc"
+                            }
+                        }
+                    ]
+                }
+            filings = query_api.get_filings(query=query)['filings']
+            if len(filings) == 0:
+                print("No filings found for this stock")
+                return None
+            
+            url = filings[0]['linkToFilingDetails']
+
+            headers = {
+                "User-Agent": "sehgal.sahil0786@gmail.com",
+                "Accept-Encoding": "gzip, deflate",
+                "Host": "www.sec.gov"
+            }
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            h = html2text.HTML2Text()
+            h.ignore_links = False
+            text = h.handle(response.content.decode('utf-8'))
+
+            text = re.sub(r"[^a-zA-Z0-9\s\n]", "", text)
+            return text
+        except requests.exceptions.HTTPError as e:
+            print(f"HTTP Error occurred: {e}")
+            return None
+        except Exception as e:
+            print(f"Error fetching 10-Q content for {stock_name}: {e}")
+            return None
+    
+    def add(self, *args:Any, **kwargs: Any)-> None:
+        """ Add the 10-Q content to the tool"""
+        kwargs["data_type"] = DataType.TEXT
+        super().add(*args, **kwargs)
+
+    def _run(self, search_query: str, **kwargs: Any) -> Any:
+        """ Perform semantic search on the 10-Q content for the given query"""
         return super()._run(search_query, **kwargs)
